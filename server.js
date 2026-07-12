b2ba28cf11cee2110a0e556bc66eb631e9331c7e
reignsystemsai <drteventbrite@gmail.com>
2026-07-12 17:10:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-Const express = require("express");
+const express = require("express");
 const http = require("http");
 const { randomUUID, createHash } = require("crypto");
 const { Pool } = require("pg");
